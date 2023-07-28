86846ff91ce0f6ca184ee5fcf636ce08b87f24a9
Mohamed Sami <srmf2025@gmail.com>
2023-07-28 16:03:05 +0530

    init: initialize remix app

---
+++ app/root.tsx
@@ -0,0 +1,37 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import stylesheet from "~/styles/globals.css";
+import type { LinksFunction } from "@remix-run/node";
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: stylesheet,
+  },
+];
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+        <title>env stash</title>
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/_index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="text-4xl font-bold flex min-h-screen justify-center items-center">
+      env stash
+    </div>
+  );
+}
